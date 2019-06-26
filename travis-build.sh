26ad45f3fb914c2ec01652717992e89e490c9017
琯琯 <yzmguanguan@gmail.com>
2019-06-26 14:53:23 +0800

    Add travis ci config files

---
+++ travis-build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#### halt script on error
+set -xe
+
+echo '##### Print docker version'
+docker --version
+
+echo '##### Print environment'
+env | sort
+
+#### Build the Docker Images
+
+cp env-example .env
+cat .env
+cp docker-compose-sample.yml docker-compose.yml
+docker-compose up -d
+docker images
